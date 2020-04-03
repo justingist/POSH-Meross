87c62a20cc2e7d51c86633181292196a19d2d641
justingist <justin@justingist.com>
2020-04-03 11:18:04 +0100

    Create POSH-Meross.psm1

---
+++ POSH-Meross.psm1
@@ -0,0 +1,18 @@
+Function Connect-MerossCloud ($credential) {
+
+
+}
+
+Function ConvertTo-MerossRequest ($data) {
+
+}
+
+Function Get-Devices ($session,$params) {
+
+}
+
+Function Get-Nonce {
+
+
+}
+
