286d6755c5eaf268fa3ce51d9a51b924fa82dec4
justingist <justin@justingist.com>
2020-04-03 11:20:41 +0100

    Update POSH-Meross.psm1

--- POSH-Meross.psm1
+++ POSH-Meross.psm1
@@ -4,7 +4,23 @@ Function Connect-MerossCloud ($credential) {
 }
 
 Function ConvertTo-MerossRequest ($data) {
+  $Bytes = [System.Text.Encoding]::ASCII.GetBytes($data)
+  $EncodedText =[Convert]::ToBase64String($Bytes)
+  $nonce = '0123456789ABCDEF' # need to improve randomness of nonce!
+  $encodeable = $secret+$timestamp_millis+$nonce+$EncodedText
+  # need to check license for stringhash function
+  $md5hash = Get-StringHash -String $encodeable
 
+
+
+  $payload = [ordered]@{
+      'params'= $EncodedText
+      'sign'= $md5hash
+      'timestamp'= $timestamp_millis
+      'nonce'= $nonce
+  }
+
+  return $payload
 }
 
 Function Get-Devices ($session,$params) {
